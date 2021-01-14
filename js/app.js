1e569b1fab8b7236590d757ec1a390a546516ebc
Oscar Silla <oscarsilla00@gmail.com>
2021-01-14 22:10:29 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function cambiarClase() {
+
+    // Obtengo el "nav" mediante el id
+    let siteNav = document.getElementById('site-nav');
+        siteNav.classList.toggle('site-nav-open');
+    
+    let menuOpen = document.getElementById('menu-toggle');
+        menuOpen.classList.toggle('menu-open');
+
+}
